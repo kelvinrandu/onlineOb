2d799185abbae75d6e407b5b1a47ee07eeb0fefd
kelvinrandu <randukelvin@gmail.com>
2018-01-30 09:34:02 +0300

    facebook login part 2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+use App\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -56,9 +57,35 @@ class LoginController extends Controller
     */
    public function handleProviderCallback()
    {
-       $user = Socialite::driver('facebook')->user();
 
-    return  $user->name;
+    $userSocial = Socialite::driver('facebook')->user();
+
+    /*get the user from the database that matches the linkedin user*/
+    // $findUser = User::whereEmail($userSocial->email)->first();
+    return  $userSocial->email;
+
+    // if($findUser){
+    //   Auth::login($userSocial);
+    //   return 'done with old';
+    //
+    // }else{
+    //
+    //   $user = new User;
+    //   $user->fname =  $userSocial->name;
+    //   $user->lname =  $userSocial->name;
+    //   $user->email =  $userSocial->email;
+    //   $user->avatar =  $userSocial->avatar;
+    //   $user->gender =  0 ;
+    //   $user->remember_token =  $userSocial->token;
+    //   $user->password =  bcrypt(12345678);
+    //   $user->save();
+    //   Auth::login($userSocial);
+    //   return 'done with new';
+    //
+    // }
+
+
+
 
    }
 
